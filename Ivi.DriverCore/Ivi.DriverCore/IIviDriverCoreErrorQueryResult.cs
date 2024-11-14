614b77ba0e5f3b1eeec48119248831d4fd36ceac
Saurav Gwalia <saurav.gwalia@non.keysight.com>
2024-11-14 13:26:50 +0530

    uncommneted

--- Ivi.DriverCore/Ivi.DriverCore/IIviDriverCoreErrorQueryResult.cs
+++ Ivi.DriverCore/Ivi.DriverCore/IIviDriverCoreErrorQueryResult.cs
@@ -6,19 +6,10 @@ using System.Threading.Tasks;
 
 namespace Ivi.DriverCore
 {
-    /// <summary>
-    /// Represents the result of an error query from an IVI driver.
-    /// </summary>
+
     public struct ErrorQueryResult
     {
-        /// <summary>
-        /// Gets the error code associated with the query.
-        /// </summary>
         Int32 Code { get; }
-
-        /// <summary>
-        /// Gets the error message associated with the query.
-        /// </summary>
         String Message { get; }
     }
 }
